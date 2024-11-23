3f61e15650ba8a8ba2c7fc8d739f2c56f40a20ec
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-23 18:44:30 +0200

    Article Add & update process with relation sync

--- app/Repositories/Article/ArticleRepository.php
+++ app/Repositories/Article/ArticleRepository.php
@@ -4,6 +4,8 @@ namespace App\Repositories\Article;
 
 use App\Models\Article\Article;
 use App\Repositories\BaseRepository;
+use Illuminate\Support\Arr;
+use Throwable;
 
 class ArticleRepository extends BaseRepository
 {
@@ -13,6 +15,33 @@ class ArticleRepository extends BaseRepository
     public function model(): string
     {
         return Article::class;
+    }
+
+
+    /**
+     * @param array $data
+     * @return mixed
+     * @throws Throwable
+     */
+    public function add(array $data): mixed
+    {
+        return $this->applyTransaction(function () use ($data) {
+            $resource = parent::add(Arr::except($data, 'categories'));
+            $resource->categories()->sync($data['categories']);
+            return $resource->loadCount('viewers')->load('categories');
+        });
+    }
+
+    /**
+     * @param $id
+     * @param array $attributes
+     * @return mixed
+     */
+    public function update($id, array $attributes): mixed
+    {
+        $resource =  parent::update($id, Arr::except($attributes, 'categories'));
+        $resource->categories()->sync($attributes['categories']);
+        return $resource->loadCount('viewers')->load('categories');
     }
 
 }
