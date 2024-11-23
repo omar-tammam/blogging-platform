c84383f804d9c9952bdf60a555c89ae64f39f29e
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-23 17:14:06 +0200

    Update 2024_11_23_113631_create_articles_table.php

--- database/migrations/2024_11_23_113631_create_articles_table.php
+++ database/migrations/2024_11_23_113631_create_articles_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
+            $table->id();
             $table->string('title');
             $table->text('content');
             $table->unsignedBigInteger('created_by');
