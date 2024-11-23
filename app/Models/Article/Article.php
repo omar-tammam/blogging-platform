1f474768131da544dc3835a31b618a32a475f0c9
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-23 16:29:54 +0200

    Create Article Model

---
+++ app/Models/Article/Article.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Models\Article;
+
+use App\Models\Category\Category;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Article extends Model
+{
+    use HasFactory;
+
+
+    protected $fillable = [
+        'title',
+        'content',
+        'created_by',
+        'updated_by',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'created_by');
+    }
+
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class, 'article_category', 'article_id', 'category_id');
+    }
+
+    public function viewers(): HasMany
+    {
+        return $this->hasMany(ArticleViewer::class, 'article_id');
+    }
+}
