97756c5489f45dec4f8160b3fde09c41f3472024
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-23 16:29:27 +0200

    build Schema Structure for catregroies, articles, articles_viewers

---
+++ database/migrations/2024_11_23_113640_create_article_viewers_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('article_viewers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('article_id');
+            $table->string('viewer_ip');
+            $table->timestamps();
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('article_viewers');
+    }
+};
