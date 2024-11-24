1ee5363b9a8a0858e83e70211cef8bc6ffc48049
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-24 07:01:10 +0200

    Create ArticleResource.php

---
+++ app/Http/Resources/User/Article/ArticleResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\User\Article;
+
+use App\Http\Resources\Admin\Category\CategoryResource;
+use Illuminate\Contracts\Support\Arrayable;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use JsonSerializable;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param Request $request
+     * @return array|Arrayable|JsonSerializable
+     */
+    public function toArray($request): array|JsonSerializable|Arrayable
+    {
+        return [
+            'id' => $this->id,
+            'title' => $this->title,
+            'categories' => CategoryResource::collection($this->categories)
+        ];
+    }
+}
