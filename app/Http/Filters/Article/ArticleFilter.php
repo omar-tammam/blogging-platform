67e2f10fca663d7f29f58c2aa9b71c25bc990129
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-24 07:03:46 +0200

    article categories & search filters

--- app/Http/Filters/Article/ArticleFilter.php
+++ app/Http/Filters/Article/ArticleFilter.php
@@ -7,5 +7,29 @@ use App\Http\Filters\Filter;
 class ArticleFilter extends Filter
 {
 
+    /**
+     * Filter by title
+     * @param array $categories
+     * @return mixed
+     */
+    public function categories(array $categories): mixed
+    {
+        if (empty($categories)) {
+            return $this->builder;
+        }
+
+        return $this->builder->whereHas('categories', function ($query) use ($categories) {
+            $query->whereIn('id', $categories);
+        });
+    }
+
+    /**
+     * @param string $search
+     * @return mixed
+     */
+    public function search(string $search): mixed
+    {
+        return $this->builder->where('title', 'like', "%$search%");
+    }
 
 }
