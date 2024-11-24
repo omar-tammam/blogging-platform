b89deb0319725deda1c1b4f776056053c5e37359
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-24 07:04:23 +0200

    make ArticleViewer  filterable

--- app/Models/Article/ArticleViewer.php
+++ app/Models/Article/ArticleViewer.php
@@ -2,12 +2,13 @@
 
 namespace App\Models\Article;
 
+use App\Traits\Filterable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class ArticleViewer extends Model
 {
-    use HasFactory;
+    use HasFactory, Filterable;
 
 
     protected $fillable = [
