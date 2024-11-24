c5e3c37ebb3fc3347d8f61d7def2285ed4ee56f7
omar tammam <69875109+omar-tammam@users.noreply.github.com>
2024-11-24 07:02:20 +0200

    articleViewers logic

--- app/Services/Article/ArticleService.php
+++ app/Services/Article/ArticleService.php
@@ -2,9 +2,11 @@
 
 namespace App\Services\Article;
 
+use App\Http\Filters\Filter;
 use App\Repositories\BaseRepository;
 use App\Repositories\Article\ArticleRepository;
 use App\Services\BaseService;
+use Throwable;
 
 class ArticleService extends BaseService
 {
@@ -20,5 +22,17 @@ class ArticleService extends BaseService
     }
 
 
+    /**
+     * @param int $page
+     * @param int $perPage
+     * @param Filter $filter
+     * @return mixed
+     */
+    public function articleViewers(int $page, int $perPage, Filter $filter): mixed
+    {
+        return app(ArticleViewerService::class)->articleViewers($page, $perPage, $filter);
+    }
 
+
+  
 }
